e6abf2d27459078d803b908730b42443c39c132a
Eric Jimenez <eric.jimenez@me.com>
2015-02-24 17:24:10 -0500

    health bar stuff

--- Assets/Scripts/ManagerScripts/GameManager.cs
+++ Assets/Scripts/ManagerScripts/GameManager.cs
@@ -3,40 +3,46 @@ using System.Collections;
 using XboxCtrlrInput;
 
 public class GameManager : MonoBehaviour {
-
+	
 	TimeManager timeManager;
 	BallSpawnManager ballManager;
 	PlayerAttributes player;
 	bool gameIsOver = false;
-
+	
+	public GameObject p1;
+	public GameObject p2;
+	public GameObject p3;
+	public GameObject p4;
+	
+	
 	void Awake() {
 		timeManager = GameObject.FindWithTag ("TimeManager").GetComponent<TimeManager> ();
 		ballManager = GameObject.FindWithTag ("BallSpawnManager").GetComponent<BallSpawnManager> ();
 		player = GameObject.FindWithTag ("Player").GetComponent<PlayerAttributes> ();
-
+		
 	}
-
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		restartGame ();
 	}
-
+	
 	void restartGame () {
-
-		if (gameIsOver && (XCI.GetButtonUp(XboxButton.Start, 2))) {
+		
+		if (gameIsOver && (XCI.GetButtonUp(XboxButton.Start, 1))) {
 			Application.LoadLevel (Application.loadedLevelName);
 		}
 	}
-
+	
 	public void gameOver () {
 		Debug.Log("Game Over");
 		timeManager.stopTimer ();
-
+		
 		GameObject[] balls = GameObject.FindGameObjectsWithTag ("Ball");
 		for(int i = 0; i < balls.Length; i++)
 		{
--- Assets/Scripts/PlayerScripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerScripts/PlayerCollision.cs
@@ -2,31 +2,52 @@
 using System.Collections;
 
 public class PlayerCollision : MonoBehaviour {
-
+	
 	GameManager gameManager;
+	
+	private float healthCount = 0.0f;
+	float immuneStartTime = 0.0f;
+	bool isImmune = false;
 
+	
 	void Awake() {
 		gameManager = GameObject.FindWithTag ("GameManager").GetComponent<GameManager> ();
 	}
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float timeDiff = Time.time - immuneStartTime;
+		if(timeDiff >= 5.0f) {
+			isImmune = false;
+		}
 	}
+
 	void OnTriggerEnter(Collider collision)
 	{
+	
 		// If the entering collider is the player...
 		if(collision.collider.gameObject.tag == "Ball")
 		{
-			gameManager.gameOver();
+			if(!isImmune) {
+				healthCount += 1.0f;
+				immuneStartTime = Time.time;
+				
+				if(healthCount >= 3) {
+					Destroy(this.gameObject);
+				}
+				
+				isImmune = true;
+				immuneStartTime = Time.time;
+			} 
 		}
+		
 		if(collision.collider.gameObject.tag == "BlackHole")
 		{
-			gameManager.gameOver();
+			Destroy(this.gameObject);
 		}
 	}
 }
