9d10d3ecee5680432727bbb8c359d26e38480534
izhi <zhi.study.ut@gmail.com>
2015-03-08 17:12:46 -0400

    menu added

---
+++ Assets/gameMeun/meun_Script/load_on_click.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class load_on_click : MonoBehaviour {
+
+	public void loadScene(string which_scene)
+	{
+		Application.LoadLevel(which_scene);
+	}
+
+}
