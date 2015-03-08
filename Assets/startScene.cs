d50926501d0b5a654bd323d0c3f83f6f07413aa8
Jackson Hong <jacksonxxhong@gmail.com>
2015-03-08 19:37:11 -0400

    start screen good to go

---
+++ Assets/startScene.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using XboxCtrlrInput;
+
+public class startScene : MonoBehaviour {
+
+	string sceneToLoad = "scene4";
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (XCI.GetButtonUp(XboxButton.Start, 1)) {
+			Application.LoadLevel(sceneToLoad);
+		}
+		if (XCI.GetButtonUp(XboxButton.Start, 2)) {
+			Application.LoadLevel(sceneToLoad);
+		}	
+		if (XCI.GetButtonUp(XboxButton.Start, 3)) {
+			Application.LoadLevel(sceneToLoad);
+		}
+		if (XCI.GetButtonUp(XboxButton.Start, 4)) {
+			Application.LoadLevel(sceneToLoad);
+		}
+	}
+}
