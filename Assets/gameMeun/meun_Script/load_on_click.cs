71aa21c82c80cd28b4981f53cd7ef97c1fe7d228
Jackson Hong <jacksonxxhong@gmail.com>
2015-03-08 19:20:41 -0400

    working with start button

--- Assets/gameMeun/meun_Script/load_on_click.cs
+++ Assets/gameMeun/meun_Script/load_on_click.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using XboxCtrlrInput;
 
 public class load_on_click : MonoBehaviour {
 
+	void Update() 
+	{
+		if (XCI.GetButtonUp(XboxButton.Start, 1)) {
+			Debug.Log("Start pressed");
+			loadScene("scene4");
+		}
+	}
 	public void loadScene(string which_scene)
 	{
-//		Debug.Log ("Clicked");
+		Debug.Log ("Clicked");
 		Application.LoadLevel(which_scene);
 	}
 
