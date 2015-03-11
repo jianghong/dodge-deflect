8d8a309135c49b1ecb940895ebf9af1d12468e74
Jackson Hong <jacksonxxhong@gmail.com>
2015-03-11 00:24:57 -0400

    char loadout

--- Assets/JoinPrompt.cs
+++ Assets/JoinPrompt.cs
@@ -16,7 +16,7 @@ public class JoinPrompt : MonoBehaviour {
 	}
 	
 	public void allowStart() {
-		GetComponent<Image>().color = new Color(0, 255f, 0);
+		GetComponent<Image>().color = new Color(0.29f, 0.61f, 0f);
 		t.text = "Press A to start game";
 	}
 }
