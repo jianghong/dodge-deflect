00b53e667712925dacb48ca3470c3f668cbfda0b
Jackson Hong <jacksonxxhong@gmail.com>
2015-03-16 13:39:20 -0400

    show round text

--- Assets/countdown.cs
+++ Assets/countdown.cs
@@ -7,17 +7,28 @@ public class countdown : MonoBehaviour {
 	Text t;
 	public int startTime = 3;
 	public int currTime = 3;
+	float awakeTime;
 	public string startText = "GO!";
+	public int roundCount;
+	GameManager gm;
+	float showRoundDelay = 1.5f;
 	bool oneTime = false;
 	// Use this for initialization
 	void Awake() {
 		t = GetComponent<Text> ();
+		gm = GameObject.FindWithTag ("GameManager").GetComponent<GameManager> ();
 		ms = GameObject.FindWithTag ("MainSceneManager").GetComponent<MainScene> ();
 		Debug.Log ("countdown");
+		awakeTime = Time.timeSinceLevelLoad;
 	}
 
 	void Start () {
-
+		if (gm.isFinalRound) {
+			t.text = "Final Round!";
+		} else {
+			roundCount = 4 - gm.roundCount;
+			t.text = "Round " + roundCount.ToString();
+		}
 	}
 	
 	// Update is called once per frame
@@ -27,8 +38,8 @@ public class countdown : MonoBehaviour {
 			t.text = startText;
 //			t.enabled = false;
 			ms.beginGame();
-		} else if (!oneTime) {
-			currTime = startTime - Mathf.RoundToInt (Time.timeSinceLevelLoad);
+		} else if (!oneTime && (Time.timeSinceLevelLoad - awakeTime) > showRoundDelay) {
+			currTime = startTime - Mathf.RoundToInt (Time.timeSinceLevelLoad-showRoundDelay);
 			t.text = currTime.ToString ();
 		}
 
