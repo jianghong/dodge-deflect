5fbbe1e183d9e3ba789ea32ebab89044fe55d730
Jackson Hong <jacksonxxhong@gmail.com>
2015-03-24 21:07:31 -0400

    void formation

--- Assets/VoidIndicator.cs
+++ Assets/VoidIndicator.cs
@@ -5,9 +5,7 @@ public class VoidIndicator : MonoBehaviour {
 
 	public float timeTilVoid = 1f;
 	public GameObject Void;
-	float lerpRate = 1f;
 	float timeSpawned;
-	public Vector3 lerpTargetScale = new Vector3(10f, 0f, 10f);
 	// Use this for initialization
 	void Start () {
 		timeSpawned = Time.time;
@@ -19,8 +17,5 @@ public class VoidIndicator : MonoBehaviour {
 			Instantiate (Void, new Vector3 (this.transform.position.x, 0.5f, this.transform.position.z), Quaternion.identity);
 			Destroy(this.gameObject);
 		}
-
-		// lerp to target
-		this.transform.localScale = Vector3.Lerp(this.transform.localScale, lerpTargetScale, 3f * Time.deltaTime * lerpRate);
 	}
 }
