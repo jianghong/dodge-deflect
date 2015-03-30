24b1d090896f2c5acca3108dbcce6036bc4e4aa9
Jackson Hong <jacksonxxhong@gmail.com>
2015-03-30 18:30:06 -0400

    proper tips

--- Assets/TipsManager.cs
+++ Assets/TipsManager.cs
@@ -5,19 +5,19 @@ using System.Linq;
 
 public class TipsManager : MonoBehaviour {
 	string[] beginnerTips = {
-		"Win a round to enter the glorious trophy round.",
-		"Form Voids strategically by shooting a star into another",
-		"Use the Right Stick to manually aim at your desired target",
-		"Be careful of the gravity when close to voids",
-		"Catch and hold (RT) stars to infuse their trail with your color. Stars that have a trail of your color don’t damage you",
-		"Shoot a star into a Void to make it bigger and more threatening"
+		"Win a round to enter the take-all trophy round!",
+		"Use the right stick for extra precise aiming!",
+		"Avoid the voids’ gravitational pull!",
+		"Catch (RT) stars to infuse them with your color, making them harmless to you!",
+		"Shoot stars into voids to make them bigger and badder."
 	};
 
 	string[] advancedTips = {
-		"Headbutt (LT) other players into voids for instant gratification",
-		"Control the arena by infusing stars with your color so there are less things for you to avoid",
-		"Stars that bounce off the metal walls will double in speed",
-		"Time your attack right when the other player has just missed their catch/headbutt to catch them off guard"
+		"Headbutt (LT) players into voids for instant gratification.",
+		"Shoot stars into each other to form voids strategically!",
+		"Control the arena by filling it with stars of your color. Less for you to avoid!",
+		"Bounce stars off the metal to double their speed!",
+		"Catch your opponents off guard when they mistime a catch or headbutt."
 	};
 
 
